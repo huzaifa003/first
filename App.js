2619c314f7f7f7a733abab772123ede667347376
Huzaifa Imran <buntyhuzaifa3@gmail.com>
2023-09-26 13:03:11 +0500

    Commiting Section

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useEffect, useState } from 'react';
 export default function App() {
+  const [name, setName] = useState("");
+  const [changed, setChanged] = useState("");
+  const handleInput = (TextInput)=>{
+    setName(TextInput);
+    console.log(name);
+    
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TextInput onChangeText={handleInput} placeholder='Pls Enter Your Name' ></TextInput>
+      <Button onPress={()=>{setChanged(name)}} title='Click me'></Button>
+      <Text>Hi {changed}</Text>
       <StatusBar style="auto" />
     </View>
   );
